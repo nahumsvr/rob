55fe94e125347e7faaca83770a8d6686a229224a
Nahum Salvador <nahumnea@gmail.com>
2025-10-15 15:14:03 -0600

    core: Se añadió capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rob.app',
+  appName: 'rob',
+  webDir: 'dist'
+};
+
+export default config;
